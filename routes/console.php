c1cb51a54ac9fee168d7791d1d2b6a259051b3a9
teslasdev <teslasdeveloper@gmail.com>
2026-05-16 03:11:03 +0100

    pushed updated

--- routes/console.php
+++ routes/console.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Schedule;
 use App\Jobs\SetupWorkerMandatesJob;
 
@@ -13,3 +14,14 @@ Schedule::job(new SetupWorkerMandatesJob())
     ->everyFifteenMinutes()
     ->withoutOverlapping()
     ->name('setup-worker-mandates');
+
+Schedule::command('queue:work --stop-when-empty --tries=3 --timeout=120')
+    ->everyMinute()
+    ->withoutOverlapping()
+    ->name('shared-hosting-queue-worker')
+    ->before(function () {
+        Log::info('[Schedule] Queue worker started at ' . now());
+    })
+    ->after(function () {
+        Log::info('[Schedule] Queue worker finished at ' . now());
+    });
